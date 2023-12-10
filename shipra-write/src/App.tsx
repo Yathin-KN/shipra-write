ebe84025af9145bfe1bb70ac0861cc74ef2ce8af
Yathin-KN <yathin2184@gmail.com>
2023-12-10 15:28:58 +0530

    LOGIN

--- shipra-write/src/App.tsx
+++ shipra-write/src/App.tsx
@@ -1,15 +1,105 @@
-import Write from "./components/write"
-
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Write from './components/write';
+import { signIn } from './apis/signin';
+import { Loader2 } from 'lucide-react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading,setIsLoading]=useState<boolean>(false);
+  const handleLogin = async () => {
+    if (!email || !password) {
+      toast.error('Please provide both email and password.');
+      return;
+    }
+
+    try {
+      setIsLoading(true)
+      const success = await signIn(email, password);
+
+      if (success) {
+        setIsLoggedIn(true);
+        toast.success('Login successful!');
+      } else {
+        setIsLoggedIn(false);
+        toast.error('Invalid credentials. Please try again.');
+      }
+    } catch (error) {
+      setIsLoggedIn(false);
+      toast.error('Something went wrong. Please try again.');
+      console.error('Login error:', error);
+    }finally{
+      setIsLoading(false)
+    }
+  };
 
   return (
-    <>
-     <div className="text-4xl">
-       <Write/>
-     </div>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      {isLoggedIn ? (
+        <div className="w-screen h-screen flex items-center justify-center">
+          <Write />
+        </div>
+      ) : (
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="text-center text-3xl font-bold">Login</h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="button"
+                onClick={handleLogin}
+                disabled={isLoading}
+                className="group  relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 items-center"
+              >
+               {isLoading &&  <Loader2 className='animate-spin text-md mx-2'/>}
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ shipra-write/src/apis/signIn.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface SignInResponse {
+  token: string;
+  user: {
+    id: number;
+    email: string;
+  };
+}
+
+export const signIn = async (email: string, password: string): Promise<boolean> => {
+  try {
+    const response: AxiosResponse<SignInResponse> = await axios.post(
+      'https://shipra-backend.vercel.app/auth/signin',
+      {
+        email,
+        password,
+      }
+    );
+
+    if (response.status === 200) {
+      
+      return true;
+    }
+  } catch (error) {
+    console.error('Authentication error:', error);
+  }
+
+  return false;
+};
